5951e1d16e63bbde5f9d1a718219d1cebde32920
Amir Alipour <amir.alipour12127@gmail.com>
2021-08-14 18:53:59 +0430

    sidebar options completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,11 @@ import AddIcon from '@material-ui/icons/Add'
 import SidebarOption from './SidebarOption';
 import InboxIcon from '@material-ui/icons/Inbox';
 import StarIcon from '@material-ui/icons/Star';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Sidebar() {
     return (
@@ -17,6 +22,17 @@ function Sidebar() {
 
             <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected />
             <SidebarOption Icon={StarIcon} title="Starred" number={16} />
+            <SidebarOption Icon={AccessAlarmIcon} title="Snoozed" number={4} />
+            <SidebarOption Icon={LabelImportantIcon} title="Important" number={3} />
+            <SidebarOption Icon={NearMeIcon} title="Sent" number={48} />
+            <SidebarOption Icon={NoteIcon} title="Drafts" number={12} />
+            <SidebarOption Icon={ExpandMoreIcon} title="More" number={6} />
+
+            <div className={styles.sidebar__footer}>
+                <div className={styles.sidebar__footerIcons}>
+
+                </div>
+            </div>
         </div>
     )
 }
