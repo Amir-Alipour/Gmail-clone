0b5634d528d31c75ffc3d42fe15a0c3c184590a5
amir alipour <amir.alipour12127@gmail.com>
2021-08-23 20:52:43 +0430

    create and complete mail Slice

---
+++ src/features/mailSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const mailSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    sendMessageIsOpen: false
+  },
+  reducers: {
+    openSendMessage: state => {
+      state.sendMessageIsOpen = true;
+    },
+    closeSendMessage: state => {
+      state.sendMessageIsOpen = false;
+    }
+  },
+  extraReducers: {
+    
+  },
+});
+
+export const { openSendMessage, closeSendMessage } = mailSlice.actions;
+
+export const selectSendMailIsOpen = state => state.mail.sendMessageIsOpen;
+
+export default mailSlice.reducer;
